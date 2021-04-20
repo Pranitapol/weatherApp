5d6f1d4bf3c08bba44615b73c9db910027e1856e
pranita pol <pranita pol>
2021-04-20 19:47:44 +0530

    change1

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -8,20 +8,18 @@ export class HttpService {
 
   constructor(private http:HttpClient) { }
 
-
+//  https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=ee091d0197a466a265ed61ea50d0f3c4
 getWeatherByCity(val:any):Observable<any>{
-  return this.http.get<any>(`http://api.weatherstack.com/current?access_key=ec250e9fc6ae55dcc5bc47da56033b86&query=${val}`);
+  return this.http.get<any>(`https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=ee091d0197a466a265ed61ea50d0f3c4`);
  
 }
-  getPrediction(val:any){
-return this.http.get<any>(`http://api.openweathermap.org/data/2.5/forecast?q=${val}&appid=ee091d0197a466a265ed61ea50d0f3c4`);
+  getPrediction(val:any):Observable<any>{
+return this.http.get<any>(`https://api.openweathermap.org/data/2.5/forecast?q=${val}&appid=ee091d0197a466a265ed61ea50d0f3c4`);
   }
   getWeather(latitude:any,longitude:any):Observable<any>{
 
-    return this.http.get<any>(`http://api.weatherstack.com/current?access_key=ec250e9fc6ae55dcc5bc47da56033b86&query=${latitude},${longitude}`);
+    return this.http.get<any>(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=ee091d0197a466a265ed61ea50d0f3c4`);
   }
-  getWeatherBySearch(val:any){
-    return this.http.get<any>(`http://api.weatherstack.com/current?access_key=ec250e9fc6ae55dcc5bc47da56033b86&query=${val}`)
-  }
+ 
 
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { Router } from "@angular/router";
-import {SettingsComponent} from '../settings/settings.component'
+import { SettingsComponent } from '../settings/settings.component'
 //import {LocalStorageService  } from "ngx-webstorage";
 @Component({
   selector: 'app-weather',
@@ -13,30 +13,29 @@ export class WeatherComponent implements OnInit {
   weather: any;
   longitude: any;
   latitude: any;
-//  val: any;
+  //  val: any;
   search: any;
-  localData:any;
- forcast:any;
-  datafor:Array<any>=[];
+  localData: any;
+  forcast: any;
+  datafor: Array<any> = [];
 
-key:any;
-item:any;
+  key: any;
+  item: any;
   constructor(private weatherservice: HttpService, private router: Router) { }
 
   ngOnInit(): void {
     this.getCurrentLocation();
 
-    for(let i=0;i<localStorage.length;i++){
-      this.key=localStorage.key(i);
-      this.item=localStorage.getItem(this.key);
-      var x:any=document.getElementById('mySelect');
-  let option=document.createElement("option");
-  option.textContent=this.item;
-  x.add(option);
-  localStorage.setItem(this.item,this.item);
-     }    
-    //this.localData=JSON.parse(String(localStorage.getItem("location")));
-    //console.log(this.localData);
+    for (let i = 0; i < localStorage.length; i++) {
+      this.key = localStorage.key(i);
+      this.item = localStorage.getItem(this.key);
+      var x: any = document.getElementById('mySelect');
+      let option = document.createElement("option");
+      option.textContent = this.item;
+      x.add(option);
+      localStorage.setItem(this.item, this.item);
+    }
+
   }
   //get latitude and longitude of current location by geolocation
   getCurrentLocation() {
@@ -50,7 +49,7 @@ item:any;
     else {
       alert("Geolocation is not supported by this browser.");
     }
-    
+
   }
   getData: any;
   //getting weather by selected city
@@ -65,20 +64,21 @@ item:any;
 
       })
 
-      
+
     }
-    if(this.selectedValue){
-      this.weatherservice.getPrediction(this.selectedValue).subscribe(data=>{
-        this.forcast=data;
+    //forecast data
+    if (this.selectedValue) {
+      this.weatherservice.getPrediction(this.selectedValue).subscribe(data => {
+        this.forcast = data;
         console.log(this.forcast);
-        for(let index=0;index<this.forcast.list.length;index++ ){
-          if(index%8==0){
+        for (let index = 0; index < this.forcast.list.length; index++) {
+          if (index % 8 == 0) {
             console.log(this.forcast.list[index]);
             this.datafor.push(this.forcast.list[index]);
           }
         }
       })
-      
+
     }
     //passing longitude and latitude to get weather details of current location
     this.weatherservice.getWeather(this.latitude, this.longitude).subscribe(data => {
@@ -89,21 +89,7 @@ item:any;
 
 
   }
-  //user can search city and get weather data
-  /*onSearch(val: any) {
-    if (val) {
-      this.weatherservice.getWeatherBySearch(val).subscribe(data => {
-        this.search = data;
-        console.log(this.search);
 
-      })
-    }
-    var x:any=document.getElementById('mySelect');
-  let option=document.createElement("option");
-  option.text=val;
-  x.add(option);
-  }
-*/
   onClick() {
     this.router.navigate(['/setting']);
   }
